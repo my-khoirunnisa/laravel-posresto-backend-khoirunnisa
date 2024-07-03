c48e48034e759a7c7e4705fdae20c6d2f1a6d1f7
my-khoirunnisa <work.khoirunnisa@gmail.com>
2024-07-03 13:00:18 +0700

    Auth (Login Register) | CRUD User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'Test Nisa User',
+            'email' => 'nisa@efic14.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin'
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Dashboard
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
+Route::middleware(['auth'])->group(function(){
+    Route::get('home', function() {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    })->name('home');
+    Route::resource('users', UserController::class);
+});
+// Route::get('/login', function () {
+//     return view('pages.auth.login', ['type_menu' => 'auth'] );
+// });
